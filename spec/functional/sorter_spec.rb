90ed24e2f37e01ddaa83ddf4ce9edb13d4f4f3b8
Mihir Kumar Thakur <mihir.thakur@e2open.com>
2021-12-12 21:32:54 +0530

    Adds more specs to sorter

--- spec/functional/sorter_spec.rb
+++ spec/functional/sorter_spec.rb
@@ -11,16 +11,22 @@ RSpec.describe 'Sorter Functional Test' do
       ]
     end
 
-    let(:sort_by_index) { 0 }
-
     it 'sorts the array according to element index' do
-      sorted_array = Sorter.sort!(array, sort_by_index)
-
+      sorted_array = Sorter.sort!(array, sort_by_index = 0)
       expect(sorted_array).to eq [
         ["Elliot", "New York City", "5/4/1947"],
         ["Mckayla", "Atlanta", "5/29/1986"],
         ["Rhiannon", "Los Angeles", "4/30/1974"],
         ["Rigoberto", "New York City", "1/5/1962"]
+      ]
+
+
+      sorted_array = Sorter.sort!(array, sort_by_index = 1)
+      expect(sorted_array).to eq [
+        ["Mckayla", "Atlanta", "5/29/1986"],
+        ["Rhiannon", "Los Angeles", "4/30/1974"],
+        ["Rigoberto", "New York City", "1/5/1962"],
+        ["Elliot", "New York City", "5/4/1947"]
       ]
     end
   end
